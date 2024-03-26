40d13e1a5345d55f5705b123870467601e815fcd
juliealhosh <juliealhosh@gmail.com>
2024-03-25 20:43:06 -0400

    deleted all files

--- data/headerNavLinks.js
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  // { href: '/blog', title: 'Recaps' },
-  // { href: '/columns', title: 'Column' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-]
-
-export default headerNavLinks
