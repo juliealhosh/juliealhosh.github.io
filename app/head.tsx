b997ac5392e09d52bab156b1a1c494bbdcf227ae
juliealhosh <juliealhosh@gmail.com>
2024-03-30 17:56:41 -0400

    Changed head.tsx (removed <meta>)

--- app/head.tsx
+++ app/head.tsx
@@ -5,10 +5,10 @@ export default function Head() {
       <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      {/* <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
-      {/* <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
+      <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
       <meta content="width=device-width, initial-scale=1" name="viewport" />
     </>
   )
