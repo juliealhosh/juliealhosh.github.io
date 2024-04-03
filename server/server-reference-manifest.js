7558b2973d59b4ad1c358b9cda3f67d52ac21a61
juliealhosh <juliealhosh@gmail.com>
2024-04-03 15:20:26 -0400

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"MkwB3dVJsFLtNnVKVwEw/5dNWDPTNcykXmvx8eV1jd4=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"66MHGVfCEVv1LfAnRyNXw6FZhDNokhqxBAtAqilvgfg=\"}"
