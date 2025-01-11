2069317657b32f1616e16fe8bc5ba81aebbf7827
juliealhosh <juliealhosh@gmail.com>
2025-01-11 14:44:42 -0500

    Added copyright to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,14 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
+import React from 'react';
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <div className="mt-16 flex flex-col items-center">
-        <div className="mb-6 flex space-x-4">
+      <div className="mt-16 mb-16 flex flex-col items-center">
+        <div className="mb-4 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="googlescholar" href={siteMetadata.googlescholar} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
@@ -17,7 +19,8 @@ export default function Footer() {
           <SocialIcon kind="rss_icon" href={siteMetadata.rss} size={6} />
           <SocialIcon kind="keyoxide" href={siteMetadata.keyoxide} size={6} /> */}
         </div>
+        <p className="text-sm opacity-50">@{currentYear} Julie Alhosh</p>
       </div>
     </footer>
-  )
+  );
 }
