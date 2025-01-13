e9b9bd769b2fa6807bbaa0ae02d6c2481da1134a
juliealhosh <juliealhosh@gmail.com>
2025-01-12 19:36:12 -0500

    ran gh-pages.sh

---
+++ gh-pages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Step 1: Replace "/_next/static" with "/static" in all .html files in "server/app"
+find server/app -type f -name "*.html" -exec sed -i 's|/_next/static|/static|g' {} \;
+
+# Step 2: Move all .html files from "server/app" to the current directory
+find server/app -type f -name "*.html" -exec cp {} . \;
+
+# Step 3: Download all files from the GitHub repository and install them into the "/static" subdirectory
+
+# Create /static directory if it doesn't exist
+mkdir -p ./static
+mkdir -p ./blog
+mkdir -p ./tags
+
+
+# Clone the repository's public/static folder into the /static directory
+git clone --depth 1 --branch main https://github.com/juliealhosh/juliealhosh.github.io.git temp_repo
+
+# Move the files from the cloned repo's "public/static" folder into your current /static directory
+mv temp_repo/public/static/* ./static/
+
+mv robotics.html conference.html ./tags/
+mv icra2024.html ./blog/
+# Clean up by removing the temporary cloned repository
+rm -rf temp_repo
+
+echo "Script execution complete."
